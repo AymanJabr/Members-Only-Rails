194bddd4fa27032b1dc6f8c258aba787b74e050b
FrederickMih <fredtimermih@gmail.com>
2021-03-23 02:48:23 +0100

    Updated the post_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,12 +4,29 @@ class PostsController < ApplicationController
       @post = Post.new
    end
 
-   def create
-      @post = Post.new 
-   end
    def index
-      
+      @post = Post.new 
+      @post.user_id = current_user.id
    end
+
+   def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    
+
+    if @post.save
+      @posts = Post.all
+      render :index
+    else
+      render :new
+    end
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:body, :user_id)
+    end
+
 
   
 end
