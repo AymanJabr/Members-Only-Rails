ee5482f56ad5e3b882ed7f1d1a3486d9397c6a59
ayman <aymanjaber2012@gmail.com>
2021-03-24 19:16:21 +0200

    Fix some linter issues

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root :to => 'posts#new' 
+  root 'posts#new'
   devise_for :users
   resources :users
   resources :posts, only: %i[index create new]
-
 end
 # rubocop:enable Style/FrozenStringLiteralComment
