89bc6cedd97d37e309caf5a934afdabc72f468c6
ayman <aymanjaber2012@gmail.com>
2021-03-25 09:21:25 +0200

    Add methods to helpers

--- app/helpers/post_helper.rb
+++ app/helpers/post_helper.rb
@@ -2,5 +2,26 @@
 # frozen_string_literal: true
 
 module PostHelper
+
+    def sign_in_or_out
+
+     if user_signed_in? 
+           link_to("Sign out", destroy_user_session_path, :method => :delete) 
+     else 
+           link_to("Sign in", new_post_path) 
+      end 
+    end
+
+    def show_right_user
+         if user_signed_in? 
+           post.user.name 
+        else 
+           "Anonymous"
+        end 
+    end
+
+
+  
+
 end
 # rubocop:enable Style/FrozenStringLiteralComment
